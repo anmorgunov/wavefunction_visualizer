f20631efafa4a4b4e0f8022d9cf73223a47a7ffb
Anton Morgunov <morgunov@mit.edu>
2022-10-11 01:16:32 -0400

    FEAT: plot Psi^2 as surfaces

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import calculatewf
 import heatmap 
+import surface
 
 grid = calculatewf.Grid('geometries/co.xyz', 5)
-values = grid.fill_mo(7)
+i = 0
+values = grid.fill_mo(i)
 
-HM = heatmap.Heatmap()
-HM.main()
-HM.plot(values, grid.X, grid.Y)
-
+# HM = heatmap.Heatmap()
+# HM.main()
+# HM.plot(values, grid.X, grid.Y)
+SF = surface.Surface()
+SF.main()
+# SF.plot(values, grid.X, grid.Y, f'mo{i+1}')
---
+++ surface.py
@@ -0,0 +1,43 @@
+from graph import Graph
+import plotly.graph_objects as go
+import numpy as np
+
+
+class Surface(Graph):
+
+    def __init__(self):
+        pass
+    
+    def plot(self, values, x, y, fname):
+        # minval = np.min(values)
+        # maxval = max(np.max(values), abs(np.min(values)))
+        # zmin, zmax = -maxval, maxval
+        # if minval >= 0:
+        #     colorscale = [[0, 'black'], [1, 'cyan']]
+        #     zmin = 0
+        # else:
+        #     colorscale = [[0, 'yellow'], [0.5, 'black'], [1, 'cyan']]
+        values = np.power(values, 2)
+        fig = go.Figure(data=[go.Surface(z=values,
+                                 x=x, y=y,
+                                 )],)
+        # fig = go.Figure(data=go.Heatmap(
+        #             z=values.flatten(),
+        #             x=x.flatten(), y=y.flatten(),
+        #             colorscale=colorscale,
+        #             zmin = zmin, zmax=zmax
+        #             ))
+        
+        self._update_fig(fig)
+        fig.write_html(f'{fname}.html', include_plotlyjs='cdn')
+
+    def main(self):
+        pass 
+
+
+
+
+if __name__ == "__main__":
+    SF = Surface()
+    SF.main()
+    # HM.plot(z, X, Y)

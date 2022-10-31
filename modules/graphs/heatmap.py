FIX: fix scaling

modules/graphs/heatmap.py
@@ -47,7 +47,7 @@ class Heatmap(Graph):
                             horizontal_spacing=0.05,
                             vertical_spacing=0.05)
         minval = min([np.min(values) for values in data])
-        maxval = min([np.max(values) for values in data])
+        maxval = max([np.max(values) for values in data])
         # print([np.max(values[1]) for values in data])
 
         # print(fig.print_grid)

tools/hydrogenwf.py
@@ -49,7 +49,8 @@ class HydrogenWF:
     def psi310(self, x, y, z):
         r = self.find_r(x, y, z)
         theta = self.find_theta(x, y)
-        rho = self.Z*r/self.A0
+        rho = self.Z*r/(self.A0)
+        # return 1/(9*np.sqrt(6)) * rho * (4-rho) * self.Z**(1.5) * np.exp(-rho/2) * np.sqrt(3) * x / r * 1/np.sqrt(4*np.pi)
         return 1/(81*np.sqrt(np.pi/2)) * (self.Z/self.A0)**1.5 * np.exp(-rho/3) * (6*rho - (rho**2)) * np.cos(theta)
 
     def psi320(self, x, y, z):